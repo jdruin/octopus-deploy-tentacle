b4d5026e5eb846b81a4e38f03983dde95f54511a
Jeffrey Druin <jdruin1@gmail.com>
2018-03-12 11:01:18 -0500

    Bringing this fork internal

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 
 default['octopus_deploy']['source'] = nil
+default['octopus_deploy']['version'] = '3.17.0'
+default['octopus_deploy']['server']['api_key'] = ''
+default['octopus_deploy']['server']['thumbprint'] = ''
+default['octopus_deploy']['server']['url'] = ''
+default['octopus_deploy']['client']['polling'] = false
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,5 +2,20 @@
 # Cookbook:: od-tentacle
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Jeffrey Druin, All Rights Reserved.
 
+od_tentacle 'Tentacle' do
+  action :configure
+  version node['octopus_deploy']['version']
+  server_thumbprint node['octopus_deploy']['server']['thumbprint']
+  api_key node['octopus_deploy']['server']['api_key']
+  polling ['octopus_deploy']['client']['polling']
+  server node['octopus_deploy']['server']['url']
+end
+
+od_tentacle 'Tentacle' do
+  action :register
+  server node['octopus_deploy']['server']['url']
+  api_key node['octopus_deploy']['server']['api_key']
+  roles node['roles']
+end
